3dc078f12da73e35de974ad3f9fd6e1c130dc215
Josh H <josh.terance@gmail.com>
2021-10-28 16:15:37 -0400

    db name correction

--- server/config/db.js
+++ server/config/db.js
@@ -4,5 +4,5 @@
     WebApp Name: COMP229-F2021-MIDTERM-301186067 */
 module.exports = {
   // REMOTE MongoDB connection
-  "URI": "mongodb+srv://jhardi11:cDNCJu0U9EfUa93u@comp229-midterm.c0oe7.mongodb.net/COMP229-MID?retryWrites=true&w=majority"
+  "URI": "mongodb+srv://jhardi11:cDNCJu0U9EfUa93u@comp229-midterm.c0oe7.mongodb.net/COMP229-MIDTERM?retryWrites=true&w=majority"
 };
